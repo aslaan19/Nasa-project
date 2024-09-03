736d4d9c698e2737d8595052859bbe96b49218e1
Abdullah Aslan <abullahaslan3010@gmail.com>
2024-09-03 05:46:35 +0300

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer>
+        <div>
       <h2>Here we have a picture of the moon</h2>
       <h1>NASA TANTA</h1>
+      </div>
       <button>
       <i className="fa-solid fa-circle-info"></i>
       </button>
+ 
     </footer>
   )
 }
